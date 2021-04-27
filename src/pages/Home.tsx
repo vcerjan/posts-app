c5664ae241c9ffe847dfe24f8c4b50f22e2ce1bd
Alkemichar <alkemichar@pop-os.localdomain>
2021-04-27 21:13:58 +0200

    feat(Home): add home page with componentGesture call and some styles

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { componentGesture } from '../services/utility'
+
+import styles from './home.module.css'
+
+export const Home: React.FC = () => {
+  componentGesture(Home.name)
+
+  return (
+    <div className={styles.container}>
+      <h1>Welcome to the Home page</h1>
+    </div>
+  )
+}
