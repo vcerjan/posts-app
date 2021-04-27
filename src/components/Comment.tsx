5d8f80f2aacdb28cdf531998a5d1b934076f6caa
Alkemichar <alkemichar@pop-os.localdomain>
2021-04-27 21:21:33 +0200

    feat(Comment): create Comment component

---
+++ src/components/Comment.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { ICommentProps } from '../interfaces/interfaces'
+import { componentGesture } from '../services/utility'
+import styles from '../styles/common.module.css'
+
+const getCommenterName = (email: string) =>
+  email.substring(0, email.indexOf('@'))
+
+export const Comment: React.FC<ICommentProps> = ({ comment }) => {
+  componentGesture(Comment.name)
+  
+  return (
+    <div className={styles.wrapper}>
+     <div className={styles.postCol}>
+       <p className={styles.commenterName}>
+        Comment by: {getCommenterName(comment.email)}
+      </p>
+      <p className={styles.commentBody}>
+        {comment.body}
+      </p>
+     </div>
+    </div>
+  )
+} 
