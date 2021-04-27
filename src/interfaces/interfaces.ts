250ee6bcdb6d71aa18e4bf0afc24e2587ec3e924
Alkemichar <alkemichar@pop-os.localdomain>
2021-04-27 21:15:13 +0200

    chore(interfaces): create base data model interfaces

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,48 @@
+import React from "react";
+
+export interface IPost {
+  id: number
+  userId: number
+  body: string
+  title: string
+}
+
+export interface IComment {
+  id: number
+  postId: number
+  body: string
+  name: string
+  email: string
+}
+
+export interface IUser {
+  id: number
+  name: string
+  username: string
+  email: string
+  address: {
+    street: string
+    suite: string
+    city: string
+    zipcode: string
+    geo: {
+      lat: number
+      lng: number
+    }
+  }
+}
+
+export interface ICommentProps {
+  comment: IComment
+}
+
+export interface IPostProps {
+  post: IPost
+  userName?: string
+  hasLink?: boolean
+  hasBody?: boolean
+}
+
+export interface IUserProps {
+  user: IUser
+}
