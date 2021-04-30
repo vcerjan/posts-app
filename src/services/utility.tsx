486aac0a0e33bc7f6cffc307f30b6ea4f0eb1150
vcerjan <vjekoslav.cerjan@gmail.com>
2021-04-30 19:20:24 +0200

    fix(utility): add missing gesture argument

--- src/services/utility.tsx
+++ src/services/utility.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 
-export const componentGesture = (componentName: string | undefined) => {
-  useEffect(() => console.log(`Hello from ${componentName}`)),
-  [componentName]
+export const componentGesture = (gesture: string, componentName: string | undefined) => {
+  useEffect(() => console.log(`${gesture} ${componentName}`)),
+  [gesture, componentName]
 }
